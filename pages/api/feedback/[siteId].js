91e6086c8280cc39dafdf9390d37ad5594a7e694
SirHexxus <jamesmichaelstacy@gmail.com>
2021-02-22 16:34:13 -0800

    updated to set up logging to logflare

--- pages/api/feedback/[siteId].js
+++ pages/api/feedback/[siteId].js
@@ -7,6 +7,19 @@ export default async (req, res) => {
 
     res.status(200).json({ feedback });
   } catch (error) {
+    logger.error(
+      {
+        request: {
+          headers: formatObjectKeys(req.headers),
+          url: req.url,
+          method: req.method
+        },
+        response: {
+          statusCode: res.statusCode
+        }
+      },
+      error.message
+    );
     res.status(500).json({ error });
   }
 };
